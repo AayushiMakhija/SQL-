6415a7b7b34c13b1f295957ba477e59270472467
AayushiMakhija <154810679+AayushiMakhija@users.noreply.github.com>
2025-06-18 15:54:21 +0530

    Implemented

--- Built-in Functions in SQL/Numeric_Functions.sql
+++ Built-in Functions in SQL/Numeric_Functions.sql
@@ -4,10 +4,10 @@
 
 SELECT 3.516 , ROUND(3.516,2) AS round_2,
 ROUND(3.516,1) AS round_1,
-ROUND(3.516,0) AS round_0
+ROUND(3.516,0) AS round_0;
 
 -- ABS()
 
 -- Take a static negative value and make it to absolute value.
 
-SELECT -10 AS negative_value, ABS(-10) AS Absolute_value
+SELECT -10 AS negative_value, ABS(-10) AS Absolute_value;
